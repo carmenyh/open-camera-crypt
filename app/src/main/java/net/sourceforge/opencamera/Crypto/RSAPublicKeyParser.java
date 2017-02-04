2932a9e5f848444978b3f96b618a58d408c116ec
CARMEN Y HANISH <carmenyh@cs.washington.edu>
2017-02-04 12:56:56 -0800

    Pem parsing for android

--- app/src/main/java/net/sourceforge/opencamera/Crypto/RSAPublicKeyParser.java
+++ app/src/main/java/net/sourceforge/opencamera/Crypto/RSAPublicKeyParser.java
@@ -1,8 +1,22 @@
 package net.sourceforge.opencamera.Crypto;
 
-import org.spongycastle.crypto.params.AsymmetricKeyParameter;
-import org.spongycastle.crypto.util.PublicKeyFactory;
 import org.spongycastle.util.io.pem.PemObject;
+import org.spongycastle.util.io.pem.PemReader;
+import org.spongycastle.util.io.pem.PemWriter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 
 /**
  * Created by bgardon on 4/02/17.
@@ -10,13 +24,18 @@ import org.spongycastle.util.io.pem.PemObject;
 
 // Parses the encoded form of an asymmetric key to a more useful format
 public class RSAPublicKeyParser {
-    public static AsymmetricKeyParameter parse(byte [] keyEncoding) {
+    public static PublicKey parse(byte [] keyEncoding) {
         // TODO implement this
         //PemReader pemReader = new PemReader(new InputStreamReader(new ByteArrayInputStream(this.asymKey)));
         //PemObject pem = pemReader.readPemObject();
 
         //PublicKeyFactory keyFactory = new PublicKeyFactory();
         //AsymmetricKeyParameter publicKey = keyFactory.createKey(asymKey);
-        return null;
+	try {
+	    return KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(keyEncoding));
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+	return null;
     }
 }
