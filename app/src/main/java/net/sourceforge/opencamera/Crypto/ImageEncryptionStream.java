e15bba749e6ab7e9cce26e25a1b9ea1f75914268
bgardon <benbwgardon@gmail.com>
2017-02-03 16:35:53 -0800

    Compiles now but I'm not sure that the initialization is done correctly

--- app/src/main/java/net/sourceforge/opencamera/Crypto/ImageEncryptionStream.java
+++ app/src/main/java/net/sourceforge/opencamera/Crypto/ImageEncryptionStream.java
@@ -2,46 +2,108 @@ package net.sourceforge.opencamera.Crypto;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.security.Key;
+import java.security.SecureRandom;
 
 import org.spongycastle.crypto.StreamCipher;
 import org.spongycastle.crypto.engines.Salsa20Engine;
 import org.spongycastle.crypto.io.CipherOutputStream;
+import org.spongycastle.crypto.params.AsymmetricKeyParameter;
+import org.spongycastle.crypto.params.KeyParameter;
+import org.spongycastle.crypto.params.ParametersWithIV;
+import org.spongycastle.crypto.util.PublicKeyFactory;
 
 /**
  * Created by bgardon on 3/02/17.
  */
 
 public class ImageEncryptionStream extends OutputStream{
-    private CipherOutputStream out;
+    private byte[] asymKey;
+    private OutputStream out;
+    private CipherOutputStream symOut;
 
-    public ImageEncryptionStream(int asymKey, int symKey, OutputStream out) {
+    public ImageEncryptionStream(byte[] asymKey, OutputStream out) {
+        if (asymKey == null || out == null) {
+            throw new IllegalArgumentException();
+        }
+        this.asymKey = asymKey;
+        this.out = out;
+    }
+
+    public void init() throws IOException {
+        if (asymKey == null || out == null) {
+            throw new IllegalStateException("Already initialized");
+        }
+        if (symOut == null) {
+            throw new IllegalStateException("Already closed");
+        }
+        // Setup a cipher and ouput stream for encrypting the symmetric key and initialization vector
+        PublicKeyFactory keyFactory = new PublicKeyFactory();
+        AsymmetricKeyParameter publicKey = keyFactory.createKey(asymKey);
+        StreamCipher asymCipher = new Salsa20Engine();
+        asymCipher.init(true, publicKey);
+        CipherOutputStream asymOut  = new CipherOutputStream(out, asymCipher);
+
+        this.asymKey = null;
+
+        // Generate an initialization vector and symmetric key
+        SecureRandom random = new SecureRandom();
+        byte[] symKey = new byte[32];
+        random.nextBytes(symKey);
+        byte[] iv = new byte[32];
+        random.nextBytes(iv);
+
+        // Write out the symmetric key, then the initialization vector
+        asymOut.write(symKey);
+        asymOut.write(iv);
+        asymOut.flush();
+        asymOut.close();
+
+        // Set up a cipher and ouput stream for encoding the image data
         StreamCipher cipher = new Salsa20Engine();
-        cipher.init(true, new Something);
-        this.out = new CipherOutputStream(out, cipher);
+        cipher.init(true, new ParametersWithIV(new KeyParameter(symKey), iv));
+        this.symOut = new CipherOutputStream(out, cipher);
+
+        this.out = null;
     }
 
     @Override
-    public void close() throws IOException {
-        this.out.close();
+    public void close() throws IOException  {
+        checkState();
+        this.symOut.close();
+        this.symOut = null;
     }
 
     @Override
-    public void flush() throws IOException{
-        this.out.flush();
+    public void flush() throws IOException {
+        checkState();
+        this.symOut.flush();
     }
 
     @Override
     public void write(byte[] b) throws IOException {
-        this.out.write(b);
+        checkState();
+        this.symOut.write(b);
     }
 
     @Override
     public void write(byte[] b, int off, int len) throws IOException {
-        this.out.write(b, off, len);
+        checkState();
+        this.symOut.write(b, off, len);
     }
 
     @Override
     public void write(int b) throws IOException {
-        this.out.write(b);
+        checkState();
+        this.symOut.write(b);
+    }
+
+    private void checkState() {
+        if (asymKey != null || out != null) {
+            throw new IllegalStateException("Not yet initialized");
+        }
+        if (symOut == null) {
+            throw new IllegalStateException("Already closed");
+        }
     }
 }
