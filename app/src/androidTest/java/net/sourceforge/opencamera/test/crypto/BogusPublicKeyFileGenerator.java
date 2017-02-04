ee31007523c05daa61366d9df111c3d10a6f3794
bgardon <benbwgardon@gmail.com>
2017-02-04 13:14:29 -0800

    added

---
+++ app/src/androidTest/java/net/sourceforge/opencamera/test/crypto/BogusPublicKeyFileGenerator.java
@@ -0,0 +1,11 @@
+package net.sourceforge.opencamera.test.crypto;
+
+/**
+ * Created by bgardon on 4/02/17.
+ */
+
+public class BogusPublicKeyFileGenerator {
+    public static void generateBogusPublicKey(String filename) {
+        // TODO generate a public/private key pair and then write the public key to the specified file
+    }
+}
