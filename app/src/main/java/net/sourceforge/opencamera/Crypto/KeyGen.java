9c0986ed399717bded438bf13cb8e895350ab624
CARMEN Y HANISH <carmenyh@cs.washington.edu>
2017-03-01 01:10:41 -0800

    This may or may not work... use with caution.

--- app/src/main/java/net/sourceforge/opencamera/Crypto/KeyGen.java
+++ app/src/main/java/net/sourceforge/opencamera/Crypto/KeyGen.java
@@ -18,6 +18,19 @@ import org.apache.commons.cli.ParseException;
 import org.spongycastle.util.io.pem.PemObject;
 import org.spongycastle.util.io.pem.PemWriter;
 
+import org.spongycastle.crypto.StreamCipher;
+import org.spongycastle.crypto.engines.Salsa20Engine;
+import org.spongycastle.crypto.io.CipherOutputStream;
+import org.spongycastle.crypto.params.KeyParameter;
+import org.spongycastle.crypto.params.ParametersWithIV;
+import java.io.FileOutputStream;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.CharArrayWriter;
 public class KeyGen {
 
 	public static void main(String[] args) {
@@ -26,6 +39,7 @@ public class KeyGen {
 		options.addOption("p", "public", true, "The location at which the generated public key should be stored");
 		options.addOption("s", "secret", true, "The location at which the generated private key should be stored");
 		options.addOption("d", "device", true, "The location at which a copy of the public key should be stored for the target device");
+        options.addOption("k", "key", true, "The password with which to protect the private key");
 		CommandLine commandLine;
 		try {
 			commandLine = parser.parse(options, args);
@@ -37,9 +51,10 @@ public class KeyGen {
 		String publicKeyFilename = commandLine.getOptionValue('p', "pub.pem");
 		String privateKeyFilename = commandLine.getOptionValue('s', "priv.pem");
 		String devicePublicKeyFilename = commandLine.getOptionValue('d', "pub.pem");
+        String privateKeyPasscode = commandLine.getOptionValue('k', "");
 
 		KeyPair keys = generateKeys();
-		saveKeysToComputer(keys.getPublic(), publicKeyFilename, keys.getPrivate(), privateKeyFilename);
+		saveKeysToComputer(keys.getPublic(), publicKeyFilename, keys.getPrivate(), privateKeyFilename, privateKeyPasscode);
 		saveKeyForDevice(keys.getPublic(), devicePublicKeyFilename);
 	}
 
@@ -90,12 +105,9 @@ public class KeyGen {
 	}
 	*/
 
-	public static void saveKeysToComputer(PublicKey publickey, String publicFile, PrivateKey privatekey, String privateFile) {
+	public static void saveKeysToComputer(PublicKey publickey, String publicFile, PrivateKey privatekey, String privateFile, String passcode) {
 		try {
-			PemWriter pemprivate = new PemWriter(new FileWriter(new File(privateFile)));
-			pemprivate.writeObject(new PemObject("RSA PRIVATE KEY", privatekey.getEncoded()));
-			pemprivate.flush();
-			pemprivate.close();
+			writeEncryptedPrivate(privatekey, privateFile, passcode);
 			
 			PemWriter pempublic = new PemWriter(new FileWriter(new File(publicFile)));
 			pempublic.writeObject(new PemObject("RSA PUBLIC KEY", publickey.getEncoded()));
@@ -105,6 +117,42 @@ public class KeyGen {
 			e.printStackTrace();
 		}
 	}
+
+    private static void writeEncryptedPrivate(PrivateKey privateKey, String privateFile, String privateKeyPassCode) {
+        try {
+            CharArrayWriter output = new CharArrayWriter();
+            PemWriter pempublic = new PemWriter(output);
+            pempublic.writeObject(new PemObject("RSA PRIVATE KEY", privateKey.getEncoded()));
+            pempublic.flush();
+            pempublic.close();
+            FileOutputStream file = new FileOutputStream(new File(privateFile));
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(privateKeyPassCode.getBytes("UTF-8"));
+            byte[] passcode = md.digest();
+            SecureRandom random = new SecureRandom();
+            byte[] iv = new byte[8];
+            random.nextBytes(iv);
+            file.write(iv.length);
+            file.write(iv);
+            StreamCipher cipher = new Salsa20Engine();
+            cipher.init(true, new ParametersWithIV(new KeyParameter(passcode), iv));
+            CipherOutputStream keyStream = new CipherOutputStream(file, cipher);
+            keyStream.write((new String(output.toCharArray())).getBytes());
+            keyStream.flush();
+            keyStream.close();
+
+
+
+            /*
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            */
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
 
 	public static void saveKeyForDevice(PublicKey publickey, String SDFile) {
 		try {
